205b8101bb37ea32dc62fe1104d5e72123b41c56
kevposesp <kevposesp@gmail.com>
2022-05-20 10:52:48 +0200

    actualizacion base de datos

--- db/motors_users.sql
+++ db/motors_users.sql
@@ -23,18 +23,16 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id_user` varchar(12) COLLATE utf8_spanish2_ci NOT NULL,
+  `id_user` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
   `username_user` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
   `email_user` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
-  `password_user` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `password_user` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `type_user` varchar(8) COLLATE utf8_spanish2_ci NOT NULL,
   `avatar_user` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
   `verify_email` tinyint NOT NULL,
-  `token_verify` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `token_verify` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
   PRIMARY KEY (`id_user`),
-  UNIQUE KEY `id_user_UNIQUE` (`id_user`),
-  UNIQUE KEY `username_user_UNIQUE` (`username_user`),
-  UNIQUE KEY `email_user_UNIQUE` (`email_user`)
+  UNIQUE KEY `id_user_UNIQUE` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -44,7 +42,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('47abdf670ff5','Donyet960','joaquimdaweb@gmail.com','$2y$12$KZ7blqsT7vsux605oqiPUezmBGgY/spKnXpZqU64i/mZ2quorpby6','client','http://res.cloudinary.com/kevposesp/image/upload/v1652453013/motors/users/ji1yjihxxthnrpadnbdo.jpg',0,'c86a78ba9e6a77d3ce34'),('67d7afc1b473','kevinaris84','posada772@gmail.com','$2y$12$igY50ajOnEEV5yrlqgHbOOpKtdrjR59YM52HVt0j.Q5GNAG.0jBxy','client','',1,'71c8d37d8971e0f5c634');
+INSERT INTO `users` VALUES ('47abdf670ff5','Donyet960','joaquimdaweb@gmail.com','$2y$12$KZ7blqsT7vsux605oqiPUezmBGgY/spKnXpZqU64i/mZ2quorpby6','client','http://res.cloudinary.com/kevposesp/image/upload/v1652453013/motors/users/ji1yjihxxthnrpadnbdo.jpg',0,'c86a78ba9e6a77d3ce34'),('67d7afc1b473','kevinaris84','posada772@gmail.com','$2y$12$igY50ajOnEEV5yrlqgHbOOpKtdrjR59YM52HVt0j.Q5GNAG.0jBxy','client','',1,'71c8d37d8971e0f5c634'),('github|90612916','kevposesp','https://github.com/kevposesp',NULL,'client','https://avatars.githubusercontent.com/u/90612916?v=4',1,NULL),('google-oauth2|118392949273033519997','posada772','posada772@gmail.com',NULL,'client','https://lh3.googleusercontent.com/a-/AOh14GhY3DViMLyKu26Gt4jf5a2OSGvg4PL6t1o8QMlNGg=s96-c',1,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-18 18:15:17
+-- Dump completed on 2022-05-20 10:51:20
